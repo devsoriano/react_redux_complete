b6f38fd6fa23a176e71ef56e1705b4ce348072ab
Rafael Soriano <rafa.soriano.ramirez@gmail.com>
2019-01-09 17:30:06 -0600

    React & redux, egghead

---
+++ src/components/TodoForm.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {updateCurrent, saveTodo} from '../reducers/todo'
+
+class TodoForm extends Component{
+  handleInputChange = (evt) => {
+    this.props.updateCurrent(evt.target.value)
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.saveTodo(this.props.currentTodo)
+  }
+
+  render(){
+    const {currentTodo} = this.props
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" 
+          onChange={this.handleInputChange}
+          value={currentTodo} 
+          />
+      </form>
+    )
+  }
+}
+
+export default connect(
+  (state) => ({currentTodo: state.todo.currentTodo}),
+  {updateCurrent, saveTodo}
+)(TodoForm)
